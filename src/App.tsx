03a72ed8c925ff79197e7de8930ea8c5997b78d0
benNjogu <kamandemachafi@gmail.com>
2022-12-06 16:35:33 +0300

    Building a form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Reminder from "./models/reminder";
 import ReminderList from "./components/ReminderList";
 import reminderService from "./services/reminder";
 import { useEffect, useState } from "react";
+import NewReminder from "./components/NewReminder";
 
 function App() {
   const [reminders, setReminders] = useState<Reminder[]>([]);
@@ -22,6 +23,7 @@ function App() {
 
   return (
     <div className="App">
+      <NewReminder />
       <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
